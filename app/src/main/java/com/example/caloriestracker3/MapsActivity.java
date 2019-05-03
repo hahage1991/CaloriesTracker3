790dff528db560f6814da1ed004ab6a83f07f5b2
hahage1991 <hxbzjh1991@gmail.com>
2019-05-04 00:20:11 +1000

    "with map, no api"

--- app/src/main/java/com/example/caloriestracker3/MapsActivity.java
+++ app/src/main/java/com/example/caloriestracker3/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.caloriestracker3;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -8,6 +10,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -27,8 +32,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        double lat = getLat("60 Lawn road noble park vic Australia");
+        double lon = getLng("60 Lawn road noble park vic Australia");
+        LatLng Melbourne = new LatLng(lat, lon);
+        mMap.addMarker(new MarkerOptions().position(Melbourne).title("Melbourne"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Melbourne));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(8), 2000, null);
+
     }
+
+   // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-37.962930,145.162420&radius=10000&type=park&keyword=cruise&key=AIzaSyAOUAfjByh7eSEVrv2ygMRrZo6MHLUB5og
+//https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBWIWQZFj_cDRcorR-jip_djf3HR4MTCaY
+    public double getLat(String address) {
+
+        Geocoder coder = new Geocoder(getApplicationContext());
+        try {
+            ArrayList<Address> adresses = (ArrayList<Address>) coder.getFromLocationName(address, 50);
+            for (Address add : adresses) {
+                double longitude = add.getLongitude();
+                double latitude = add.getLatitude();
+                return latitude;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+
+    public double getLng(String address) {
+
+        Geocoder coder = new Geocoder(getApplicationContext());
+        try {
+            ArrayList<Address> adresses = (ArrayList<Address>) coder.getFromLocationName(address, 50);
+            for (Address add : adresses) {
+                // if (statement) {//Controls to ensure it is right address such as country etc.
+                double longitude = add.getLongitude();
+                double latitude = add.getLatitude();
+                return longitude;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
 }

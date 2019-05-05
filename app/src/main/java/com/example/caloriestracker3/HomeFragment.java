1472174b1d27885ca1524df0846864fcc341be66
hahage1991 <hxbzjh1991@gmail.com>
2019-05-05 14:12:36 +1000

    "with map ready"

--- app/src/main/java/com/example/caloriestracker3/HomeFragment.java
+++ app/src/main/java/com/example/caloriestracker3/HomeFragment.java
@@ -13,10 +13,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 public class HomeFragment extends Fragment {
 
@@ -24,6 +39,7 @@ public class HomeFragment extends Fragment {
     private TextView tv_date;
     private TextView tv_test;
     private Button b2;
+    private JSONArray parks;
 
     // private EditText et_username;
 
@@ -32,12 +48,14 @@ public class HomeFragment extends Fragment {
 
     //private String passwordHash;
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
             savedInstanceState) {
 
         //super.onCreate(savedInstanceState);
+        findPark2();
 
         vHome = inflater.inflate(R.layout.fragment_home, container, false);
         tv_date = (TextView) vHome.findViewById(R.id.tv_date);
@@ -62,8 +80,10 @@ public class HomeFragment extends Fragment {
 
         //tv_register.setOnClickListener(this);
         //tv_test.setText("all1");
-        getConsumptionByID();
-       onClick(b2);
+       // getConsumptionByID();
+       // findPark2();
+        getParks();
+      onClick(b2);
 
         return vHome;
     }
@@ -72,13 +92,140 @@ public class HomeFragment extends Fragment {
     {
         if (v == b2)
         {
+
+         //   tv_test.setText( "hi");
             Intent map = new Intent(getActivity(),MapsActivity.class);
+           // Bundle b = new Bundle();
+            //b.putString("Array",parks.toString());
+          // map.putExtras(b);
+
              startActivity(map);
+
+
         }
         return;
 
     }
 
+    //private static final String BASE_URI = "http://118.138.108.194:8080/smartERDB/webresources";
+
+
+    public static String findPark3() {
+        //final String methodPath = "/smarter.usage/findByResid/2";
+       // final String methodPath = "/smarter.usage/";
+//initialise
+        URL url = null;
+        HttpURLConnection conn = null;
+        String textResult = "";
+//Making HTTP request
+        try {
+            url = new URL("https://maps.googleapis.com/maps/api/place/textsearch/json?&location=-37.962930,145.162420&radius=5000&type=park&key=AIzaSyAOUAfjByh7eSEVrv2ygMRrZo6MHLUB5og");
+
+//open the connection
+            conn = (HttpURLConnection) url.openConnection();
+//set the timeout
+            conn.setReadTimeout(10000);  //?
+            conn.setConnectTimeout(15000); //set the connection method to GET
+            conn.setRequestMethod("GET");
+//add http headers to set your response type to json
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json"); //Read the response
+            Scanner inStream = new Scanner(conn.getInputStream()); //read the input steream and store it as string
+            while (inStream.hasNextLine()) {
+                textResult += inStream.nextLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn.disconnect();
+        }
+        return textResult;
+
+
+    }
+
+    public void getParks() {
+        //create an anonymous AsyncTask
+        //(http://localhost:8080/NewC/webresources/newc.consumption/{id})
+        new AsyncTask<Void, Void, JSONArray>() {
+            @Override
+            protected JSONArray doInBackground(Void... params)
+            {
+                try
+                {
+                    JSONObject res = new JSONObject(findPark3());
+                    JSONArray  results = res.getJSONArray("results");
+
+
+                    return results;
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    return null;
+                }
+
+            }
+
+            @Override
+            protected void onPostExecute( JSONArray all)
+            {
+
+                try{
+
+
+                    for (int i = 0 ; i <  all.length(); i++) {
+                        JSONObject obj =  all.getJSONObject(i);
+                        JSONObject geo = obj.getJSONObject("geometry");
+                        JSONObject location = geo.getJSONObject("location");
+                        //locations.add(location);
+                        double lat = location.getDouble("lat");
+                        double lng = location.getDouble("lng");
+
+                        String name =  obj.getString("name");
+                        tv_test.setText(name);
+
+                        // String B = obj.getString("B");
+                        //String C = obj.getString("C");
+                        // System.out.println(A + " " + B + " " + C);
+                        // tv_test.setText(String.valueOf(parks));
+
+
+
+                    }
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+
+                }
+
+
+                //tv_test.setTextColor(Color.RED);
+
+
+
+            }
+        }.execute();
+    }
+
+/*
+    public static float getFrideUsage(String date,int resid)
+    {
+        float usage = 0;
+        JSONArray jsonArray = null;
+        // JSONObject oneObject = null;
+        try{
+            jsonArray = new JSONArray(findAllUsage("findDailyUsageOfAppliances/" + resid +"/" + date + "?"));
+            JSONObject oneObject =  jsonArray.getJSONObject(0);
+
+            usage = Float.valueOf(oneObject.getString("fridge"));
+
+        }catch (Exception e){ e.printStackTrace();
+            usage  = (float) 0.0;
+        }
+        return usage;
+
+    }*/
     public void getConsumptionByID() {
         //create an anonymous AsyncTask
         //(http://localhost:8080/NewC/webresources/newc.consumption/{id})
@@ -93,7 +240,7 @@ public class HomeFragment extends Fragment {
             protected void onPostExecute(String all)
             {
 
-                    tv_test.setText(all);
+                 //   tv_test.setText(all);
                     //tv_test.setTextColor(Color.RED);
 
 
@@ -101,4 +248,107 @@ public class HomeFragment extends Fragment {
             }
         }.execute();
     }
+    // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-37.962930,145.162420&radius=10000&type=park&keyword=cruise&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyAOUAfjByh7eSEVrv2ygMRrZo6MHLUB5og
+//&fields=photos,formatted_address,name,rating,opening_hours,geometry
+//https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street&location=-37.962930,145.162420&radius=10000&type=park&key=AIzaSyAOUAfjByh7eSEVrv2ygMRrZo6MHLUB5og
+    //https://maps.googleapis.com/maps/api/place/textsearch/json?&location=-37.962930,145.162420&radius=5000&type=park&key=AIzaSyAOUAfjByh7eSEVrv2ygMRrZo6MHLUB5og
+    public void findPark2(){
+
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+
+
+                // JSONArray parks1 = new JSONArray();
+                String url = "https://maps.googleapis.com/maps/api/place/textsearch/json?&location=-37.962930,145.162420&radius=5000&type=park&key=AIzaSyAOUAfjByh7eSEVrv2ygMRrZo6MHLUB5og";
+                JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray  results = response.getJSONArray("results");
+                            parks = results;
+                            JSONArray parks1 = new JSONArray();
+                           // parks1 = results;
+                            for (int i = 0 ; i <  results .length(); i++) {
+                                JSONObject obj =  results .getJSONObject(i);
+                                JSONObject geo = obj.getJSONObject("geometry");
+                                JSONObject location = geo.getJSONObject("location");
+                                //locations.add(location);
+                                double lat = location.getDouble("lat");
+                                double lng = location.getDouble("lng");
+
+                                String A =  obj.getString("name");
+
+                               // String B = obj.getString("B");
+                                //String C = obj.getString("C");
+                               // System.out.println(A + " " + B + " " + C);
+                              // tv_test.setText(String.valueOf(parks));
+
+
+
+                            }
+
+
+
+
+
+                           // JSONObject main_object = response.getJSONObject("results");
+                           // JSONObject array =
+                            //JSONArray array = response.getJSONArray("location");
+                           // JSONObject object = array.getJSONObject(0);
+                           // String lat = String.valueOf(main_object.getDouble("lat"));
+                           // String description = object.getString("description");
+                            //String city = response.getString("name");
+                           // System.out.println(String.valueOf( main_object));
+                           // System.out.println(String.valueOf( array ));
+                           // System.out.println(String.valueOf( object ));
+                         //   System.out.println(lat);
+
+
+
+
+
+
+
+                        } catch (JSONException e)
+                        {
+                            e.printStackTrace();
+
+                        }
+
+
+
+                    }
+
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+                );
+                RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+                queue.add(jor);
+                return null;
+
+               //return url;
+            }
+
+            protected void onPostExecute(String res)
+            {
+
+                  tv_test.setText(res);
+                //tv_test.setTextColor(Color.RED);
+
+
+
+            }
+
+        }.execute();
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/caloriestracker3/MapsActivity.java
+++ app/src/main/java/com/example/caloriestracker3/MapsActivity.java
@@ -2,43 +2,230 @@ package com.example.caloriestracker3;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+   // Bundle b = getIntent().getExtras();
+   // String parksString=b.getString("Array");
+
+   // public static final String LOCATION_LIST_EXTRA = "locations";
+    //private ArrayList<JSONObject> locations = (ArrayList<location>) intent.getSerializableExtra(LOCATION_LIST_EXTRA);
+
+    //ArrayList<JSONObject> locations2 = getIntent().getExtras("locations");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // findPark();
         setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
         // Add a marker in Sydney, Australia, and move the camera.
         double lat = getLat("60 Lawn road noble park vic Australia");
         double lon = getLng("60 Lawn road noble park vic Australia");
         LatLng Melbourne = new LatLng(lat, lon);
+        //LatLng Park = new LatLng(-37.9903169,  145.215391);
         mMap.addMarker(new MarkerOptions().position(Melbourne).title("Melbourne"));
+       // mMap.addMarker(new MarkerOptions().position(Park).title("Park"));
+       /*
+        try {
+            JSONArray results = new JSONArray(parksString);
+
+            //JSONArray  results = response.getJSONArray("results");
+            for (int i = 0 ; i <  results .length(); i++) {
+                JSONObject obj =  results .getJSONObject(i);
+                JSONObject geo = obj.getJSONObject("geometry");
+                JSONObject location = geo.getJSONObject("location");
+                //locations.add(location);
+                double lat2 = location.getDouble("lat");
+                double lng2 = location.getDouble("lng");
+
+                String A =  obj.getString("name");
+                LatLng newlocation = new LatLng(lat2,  lng2);
+                mMap.addMarker(new MarkerOptions().position(newlocation).title("new"));
+                // String B = obj.getString("B");
+                //String C = obj.getString("C");
+                // System.out.println(A + " " + B + " " + C);
+
+
+            }
+
+
+
+
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+
+        }*/
+
+
+            /*
+       try {
+           JSONArray arr = new JSONArray(parksString);
+            for (JSONObject location : locations2) {
+                double lat2 = location.getDouble("lat");
+                double lng2 = location.getDouble("lng");
+                LatLng newlocation = new LatLng(lat2,  lng2);
+                mMap.addMarker(new MarkerOptions().position(newlocation).title("new"));
+
+
+                //String A = location.getString("name");
+            }
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+
+        }*/
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Melbourne));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(8), 2000, null);
+        getParks();
 
+    }
+
+
+    public static String findPark3() {
+        //final String methodPath = "/smarter.usage/findByResid/2";
+        // final String methodPath = "/smarter.usage/";
+//initialise
+        URL url = null;
+        HttpURLConnection conn = null;
+        String textResult = "";
+//Making HTTP request
+        try {
+            url = new URL("https://maps.googleapis.com/maps/api/place/textsearch/json?&location=-37.962930,145.162420&radius=5000&type=park&key=AIzaSyAOUAfjByh7eSEVrv2ygMRrZo6MHLUB5og");
+
+//open the connection
+            conn = (HttpURLConnection) url.openConnection();
+//set the timeout
+            conn.setReadTimeout(10000);  //?
+            conn.setConnectTimeout(15000); //set the connection method to GET
+            conn.setRequestMethod("GET");
+//add http headers to set your response type to json
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json"); //Read the response
+            Scanner inStream = new Scanner(conn.getInputStream()); //read the input steream and store it as string
+            while (inStream.hasNextLine()) {
+                textResult += inStream.nextLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn.disconnect();
+        }
+        return textResult;
+
+
+    }
+
+    public void getParks() {
+        //create an anonymous AsyncTask
+        //(http://localhost:8080/NewC/webresources/newc.consumption/{id})
+        new AsyncTask<Void, Void, JSONArray>() {
+            @Override
+            protected JSONArray doInBackground(Void... params)
+            {
+                try
+                {
+                    JSONObject res = new JSONObject(findPark3());
+                    JSONArray  results = res.getJSONArray("results");
+
+
+                    return results;
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    return null;
+                }
+
+            }
+
+            @Override
+            protected void onPostExecute( JSONArray all)
+            {
+
+                try{
+
+
+                    for (int i = 0 ; i <  all.length(); i++) {
+                        JSONObject obj =  all.getJSONObject(i);
+                        JSONObject geo = obj.getJSONObject("geometry");
+                        JSONObject location = geo.getJSONObject("location");
+                        //locations.add(location);
+                        double lat2 = location.getDouble("lat");
+                        double lng2 = location.getDouble("lng");
+
+                        String name =  obj.getString("name");
+                       // tv_test.setText(name);
+
+                        // String B = obj.getString("B");
+                        //String C = obj.getString("C");
+                        // System.out.println(A + " " + B + " " + C);
+                        // tv_test.setText(String.valueOf(parks));
+                        LatLng newlocation = new LatLng(lat2,  lng2);
+                        mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).position(newlocation).title(name));
+
+
+
+                    }
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+
+                }
+
+
+                //tv_test.setTextColor(Color.RED);
+
+
+
+            }
+        }.execute();
     }
 
    // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-37.962930,145.162420&radius=10000&type=park&keyword=cruise&key=AIzaSyAOUAfjByh7eSEVrv2ygMRrZo6MHLUB5og
@@ -80,4 +267,74 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return 0;
     }
 
+
+
+
+    public void findPark(){
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+
+
+                String url = "https://maps.googleapis.com/maps/api/place/textsearch/json?&location=-37.962930,145.162420&radius=5000&type=park&key=AIzaSyAOUAfjByh7eSEVrv2ygMRrZo6MHLUB5og";
+                JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray  results = response.getJSONArray("results");
+                            for (int i = 0 ; i <  results .length(); i++) {
+                                JSONObject obj =  results .getJSONObject(i);
+                                JSONObject geo = obj.getJSONObject("geometry");
+                                JSONObject location = geo.getJSONObject("location");
+                                //locations.add(location);
+                                double lat = location.getDouble("lat");
+                                double lng = location.getDouble("lng");
+
+                                String A =  obj.getString("name");
+                                // String B = obj.getString("B");
+                                //String C = obj.getString("C");
+                                // System.out.println(A + " " + B + " " + C);
+
+
+                            }
+
+
+
+
+                        } catch (JSONException e)
+                        {
+                            e.printStackTrace();
+
+                        }
+
+                    }
+
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+                );
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                queue.add(jor);
+
+                return null;
+            }
+
+            protected void onPostExecute(String all)
+            {
+
+                //tv_test.setText("hii");
+                //tv_test.setTextColor(Color.RED);
+
+
+
+            }
+
+        }.execute();
+    }
 }
+
+
